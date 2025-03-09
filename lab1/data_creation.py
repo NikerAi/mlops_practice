e7504b4f73356e6b533e8a373e712b9ac800e55f
Nikita Bytsyuk <nikita.05.98@mail.ru>
2025-03-09 17:25:28 +0500

    Update data_creation.py

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -3,8 +3,8 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
-iris = load_iris(as_frame=True)
-df = iris.frame
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
 train_df, test_df = train_test_split(df, train_size=0.8, random_state=42)
 train_df.to_csv('train_dataset.csv', index=False)
 train_df.to_csv('test_dataset.csv', index=False)
